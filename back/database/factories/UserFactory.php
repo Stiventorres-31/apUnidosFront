6ee9756222abb4d562fecb36936b23f8f4d7617f
Stiven Torres <torresmendozastiven@gmail.com>
2025-01-21 11:12:04 -0500

    ADD super admin

--- back/database/factories/UserFactory.php
+++ back/database/factories/UserFactory.php
@@ -23,12 +23,19 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        // return [
+        //     'name' => fake()->name(),
+        //     'email' => fake()->unique()->safeEmail(),
+        //     'email_verified_at' => now(),
+        //     'password' => static::$password ??= Hash::make('password'),
+        //     'remember_token' => Str::random(10),
+        // ];
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'nombre_completo' => "SUPER ADMINISTRADOR",
+            'numero_identificacion' => "123456789",
+            'rol_usuario' => "SUPER ADMIN",
+            'password' => Hash::make('password'),
+            'estado' => "A",
         ];
     }
 
