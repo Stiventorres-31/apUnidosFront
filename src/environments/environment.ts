ae28388ef03a5abc9b415793307b12c49d5ccedf
Stiven Torres <torresmendozastiven@gmail.com>
2025-02-04 16:10:59 -0500

    Configuracion de rutas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    backend: "http://127.0.0.1:8000/",
-    front: "http://localhost:4200/",
+    backend: "https://apunidoback-production.up.railway.app/",
+    front: "https://apunidosfront.onrender.com/",
 }
 
 // Entorno production
